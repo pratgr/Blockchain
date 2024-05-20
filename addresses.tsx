309a6ff6140c3cf0c9948d01ab16b17fcc7e9475
pratgr <113162958+pratgr@users.noreply.github.com>
2024-05-20 15:34:59 +0530

    Add files via upload

---
+++ addresses.tsx
@@ -0,0 +1,5 @@
+export const TRANSFER_CONTRACT_ADDRESS='0x979D1FbAAaFc09fac37D081302450882c6672eA8';
+export const CLAIM_TOKEN_CONTRACT_ADDRESS='0x37855685b71d96411a3D953dd9bafE6C981899a1';
+
+export const HERO_IMAGE_URL='ipfs://QmQEZNbStrgWS5j7VbCcvZ6YMA3oFoBZSakksgdKkFPrzQ/png-transparent-coin-cryptocurrency-exchange-tether-token-trade-usdt-popular-cryptocurrencies-vol-icon.png'
+export const FEATURES_IMAGE_URL='ipfs://QmNY1vFRhy3ASRZyZRUHmQrua7XSvdf3fJkF8bBFEPEyE7/png-transparent-computer-icons-graphy-japan-features-text-photography-hand-thumbnail.png'
